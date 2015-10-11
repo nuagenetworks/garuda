c387cb3ccbb2d40426674ae300b8407308e0d374
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-10-10 21:59:42 -0700

    Update locustfile

--- garuda/tests/locustfile.py
+++ garuda/tests/locustfile.py
@@ -7,9 +7,10 @@ import json
 class UserBehavior(TaskSet):
     """
     """
-    AUTH    = 'cm9vdDo5Zjk3NmQ1MC02YTA3LTQyM2ItODAxYy1jZDJjZTQyMmYwNjE='
-    LIST_ID = 'b7c1df62-0bf3-437a-9bf6-a8822a1b1fee'
-    TASK_ID = '118f0eb4-6434-4594-b9f7-f3799ba6905c'
+    AUTH    = 'XREST cm9vdDpkMjZhYTk4NC03YzA5LTQyNmEtOTFiNS0wM2ZjNjU5ZGEwZTc='
+    LIST_ID = '3a150c5b-9529-4f5e-94da-f00fcf7e2feb'
+    TASK_ID = '01389dea-6edc-4655-b3c2-f9c72be738f7'
+    USER_ID = '79ef2f91-528b-4937-a989-cf81ca2e07c5'
 
     def on_start(self):
         pass
@@ -18,38 +19,51 @@ class UserBehavior(TaskSet):
     def lists(self):
         """
         """
-        self.client.get("/lists", headers={'Authorization': 'XREST %s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
+        self.client.get("/lists", headers={'Authorization': '%s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
+
+    @task
+    def one_list(self):
+        """
+        """
+        self.client.get("/lists/%s" % self.LIST_ID, headers={'Authorization': '%s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
 
     @task
     def users(self):
         """
         """
-        self.client.get("/users", headers={'Authorization': 'XREST %s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
+        self.client.get("/users", headers={'Authorization': '%s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
 
     @task
     def tasks_in_list(self):
         """
         """
-        self.client.get("/lists/%s/tasks" % self.LIST_ID, headers={'Authorization': 'XREST %s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
+        self.client.get("/lists/%s/tasks" % self.LIST_ID, headers={'Authorization': '%s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
 
     @task
     def users_in_task(self):
         """
         """
-        self.client.get("/tasks/%s/users" % self.TASK_ID, headers={'Authorization': 'XREST %s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
+        self.client.get("/tasks/%s/users" % self.TASK_ID, headers={'Authorization': '%s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
 
     @task
     def create_delete_list(self):
         """
         """
-        response = self.client.post("/lists", data=json.dumps({'title': 'test', 'description': 'test'}), headers={'Authorization': 'XREST %s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
+        response = self.client.post("/lists", data=json.dumps({'title': 'test', 'description': 'test'}), headers={'Authorization': '%s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
         data = json.loads(response.content)
-        self.client.delete("/lists/%s" % data[0]["ID"], headers={'Authorization': 'XREST %s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
+        self.client.delete("/lists/%s" % data[0]["ID"], headers={'Authorization': '%s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
+
+    @task
+    def assign_unassign(self):
+        """
+        """
+        response = self.client.put("/task/%s/users" % self.TASK_ID, data=json.dumps([self.USER_ID]), headers={'Authorization': '%s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
+        response = self.client.put("/task/%s/users" % self.TASK_ID, data=json.dumps([]), headers={'Authorization': '%s' % self.AUTH, 'X-Nuage-Organization':'csp'}, verify=False)
 
 
 class WebsiteUser(HttpLocust):
     """
     """
     task_set = UserBehavior
-    min_wait=10
-    max_wait=15
+    min_wait=5
+    max_wait=10
