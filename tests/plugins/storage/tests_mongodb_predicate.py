ccce812d8ad9f21bede7b5f726b7fef1609c8b17
Antoine Mercadal <antoine.mercadal@gmail.com>
2016-03-25 22:00:09 -0700

    Support for uppercase predicate operators

--- tests/plugins/storage/tests_mongodb_predicate.py
+++ tests/plugins/storage/tests_mongodb_predicate.py
@@ -23,113 +23,102 @@ class TestGAMongoPredicateConverter(TestCase):
     def test_strings(self):
         """
         """
-        self.assertConvertEquals('name = Christophe',
-                {'name': {'$eq': 'Christophe'}})
+        self.assertConvertEquals('name = Christophe', {'name': {'$eq': 'Christophe'}})
 
-        self.assertConvertEquals('name = "Christophe"',
-                {'name': {'$eq': 'Christophe'}})
+        self.assertConvertEquals('name = "Christophe"', {'name': {'$eq': 'Christophe'}})
 
-        self.assertConvertEquals('name = \'Christophe\'',
-                {'name': {'$eq': 'Christophe'}})
+        self.assertConvertEquals('name = \'Christophe\'', {'name': {'$eq': 'Christophe'}})
 
         self.assertConvertEquals('name = Christophe and team is "UI team"',
-                {'$and': [
-                    {'name': {'$eq': 'Christophe'}},
-                    {'team': {'$eq': 'UI team'}}
-                ]})
+                                 {'$and': [
+                                     {'name': {'$eq': 'Christophe'}},
+                                     {'team': {'$eq': 'UI team'}}
+                                 ]})
 
     def test_integers(self):
         """
         """
         self.assertConvertEquals('year >= 2015 and year <= 2000',
-                {'$and': [
-                    {'year': {'$gte': 2015}},
-                    {'year': {'$lte': 2000}}
-                ]})
+                                 {'$and': [
+                                     {'year': {'$gte': 2015}},
+                                     {'year': {'$lte': 2000}}
+                                 ]})
 
     def test_booleans(self):
         """
         """
-        self.assertConvertEquals('isValid is true and isArchived = false',
-                {'$and': [
-                    {'isValid': {'$eq': True}},
-                    {'isArchived': {'$eq': False}}
-                ]})
+        self.assertConvertEquals('isValid is true AND isArchived = false',
+                                 {'$and': [
+                                     {'isValid': {'$eq': True}},
+                                     {'isArchived': {'$eq': False}}
+                                 ]})
 
         self.assertConvertEquals('isValid is True and isArchived = False',
-                {'$and': [
-                    {'isValid': {'$eq': 'True'}},
-                    {'isArchived': {'$eq': 'False'}}
-                ]})
+                                 {'$and': [
+                                     {'isValid': {'$eq': True}},
+                                     {'isArchived': {'$eq': False}}
+                                 ]})
 
         self.assertConvertEquals('isValid = 0 and isArchived = 1',
-                {'$and': [
-                    {'isValid': {'$eq': 0}},
-                    {'isArchived': {'$eq': 1}}
-                ]})
+                                 {'$and': [
+                                     {'isValid': {'$eq': 0}},
+                                     {'isArchived': {'$eq': 1}}
+                                 ]})
 
     def test_constants(self):
         """
         """
-        self.assertConvertEquals('defaultValue is null',
-                    {'defaultValue': {'$eq': 'null'}})
+        self.assertConvertEquals('defaultValue is null', {'defaultValue': {'$eq': 'null'}})
 
-        self.assertConvertEquals('defaultValue is undefined',
-                    {'defaultValue': {'$eq': 'null'}})
+        self.assertConvertEquals('defaultValue is undefined', {'defaultValue': {'$eq': 'null'}})
 
-        self.assertConvertEquals('defaultValue is empty',
-                    {'defaultValue': {'$eq': ''}})
+        self.assertConvertEquals('defaultValue is empty', {'defaultValue': {'$eq': ''}})
 
     def test_operator_or(self):
         """
         """
         self.assertConvertEquals('name = Christophe or name = Antoine',
-                {'$or': [
-                    {'name': {'$eq': 'Christophe'}},
-                    {'name': {'$eq': 'Antoine'}}
-                ]})
+                                 {'$or': [
+                                     {'name': {'$eq': 'Christophe'}},
+                                     {'name': {'$eq': 'Antoine'}}
+                                 ]})
 
         self.assertConvertEquals('name = Christophe and (name = Antoine or name = Alexandre)',
-                {'$and': [
-                    {'name': {'$eq': 'Christophe'}},
-                    { '$or': [
-                        {'name': {'$eq': 'Antoine'}},
-                        {'name': {'$eq': 'Alexandre'}}
-                    ]}
-                ]})
+                                 {'$and': [
+                                     {'name': {'$eq': 'Christophe'}},
+                                     {'$or': [
+                                         {'name': {'$eq': 'Antoine'}},
+                                         {'name': {'$eq': 'Alexandre'}}
+                                     ]}
+                                 ]})
 
     def test_operator_not_equal(self):
         """
         """
-        self.assertConvertEquals('name != Christophe',
-                {'name': {'$ne': 'Christophe'}})
+        self.assertConvertEquals('name != Christophe', {'name': {'$ne': 'Christophe'}})
 
-        self.assertConvertEquals('name is not Christophe',
-                {'name': {'$ne': 'Christophe'}})
+        self.assertConvertEquals('name is not Christophe', {'name': {'$ne': 'Christophe'}})
 
     def test_operator_equal(self):
         """
         """
-        self.assertConvertEquals('name = Christophe',
-                {'name': {'$eq': 'Christophe'}})
+        self.assertConvertEquals('name = Christophe', {'name': {'$eq': 'Christophe'}})
 
-        self.assertConvertEquals("name is Christophe",
-                {'name': {'$eq': 'Christophe'}})
+        self.assertConvertEquals("name is Christophe", {'name': {'$eq': 'Christophe'}})
 
-        self.assertConvertEquals('name == Christophe',
-                {'name': {'$eq': 'Christophe'}})
+        self.assertConvertEquals('name == Christophe', {'name': {'$eq': 'Christophe'}})
 
     def test_search_id(self):
         """
         """
         self.assertConvertEquals('ID == "564d10af7ddf1f99955884ae"',
-                {'_id': {'$eq': ObjectId('564d10af7ddf1f99955884ae')}})
+                                 {'_id': {'$eq': ObjectId('564d10af7ddf1f99955884ae')}})
 
     def test_search_contains(self):
         """
         """
-        self.assertConvertEquals('name contains Chris',
-                {'name': {'$regex': 'Chris', '$options': 'i'}})
+        self.assertConvertEquals('name contains Chris', {'name': {'$regex': 'Chris', '$options': 'i'}})
+        self.assertConvertEquals('name CONTAINS Chris', {'name': {'$regex': 'Chris', '$options': 'i'}})
 
     def test_invalid_predicate(self):
         """
